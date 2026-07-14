65ad2c71177e7ca1a7b7001d6f9c68a94cc5f22f
Maria-gom <mariecheverri100@gmail.com>
2026-07-13 23:28:25 -0500

    Fix console log message in main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-console.log("¡Hola desde el futuro! JavaScript está funcionando ");
+console.log("¡Hola desde el futuro! JavaScript está funcionando!");
 
 document.querySelector("h1").addEventListener("click", function() {
   this.textContent = "¡Hiciste clic desde el futuro! ";
