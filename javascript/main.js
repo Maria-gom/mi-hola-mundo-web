c1460e1904b2a56187349368c1616cd8d7783c04
Maria-gom <mariecheverri100@gmail.com>
2026-07-13 19:14:31 -0500

    Delete javascript directory

--- javascript/main.js
+++
@@ -1,5 +0,0 @@
-console.log("¡Hola desde el futuro! JavaScript está funcionando ");
-
-document.querySelector("h1").addEventListener("click", function() {
-  this.textContent = "¡Hiciste clic desde el futuro! ";
-});
